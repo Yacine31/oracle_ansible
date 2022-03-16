07165fa828f80d4e52faa77b745ceb7794d52de2
Yacine31 <yacine.oumghar@gmail.com>
2022-03-16 01:18:11 +0100

    Update z_oracle.sh

--- roles/configure_os/files/z_oracle.sh
+++ roles/configure_os/files/z_oracle.sh
@@ -22,9 +22,9 @@ if [ $USER = "grid" ] || [ $USER = "oracle" ] || [ $USER = "root" ] ; then
         umask 022
 
         # Pour prévention SSH
-        if [ -t 0 ]; then
-            stty intr ^C
-        fi
+        # if [ -t 0 ]; then
+        #     stty intr ^C
+        # fi
 
         # Alias RL Wrap si disponible
         if [ `type rlwrapp 2>/dev/null | wc -l` -eq 1 ] ; then
@@ -34,20 +34,20 @@ if [ $USER = "grid" ] || [ $USER = "oracle" ] || [ $USER = "root" ] ; then
             alias adrci="rlwrap adrci"      
             alias dgmgrl="rlwrap dgmgrl"
         fi
+    fi
         
-        # eviter le prompt si ce n'est pas une connexion ssh en tty
-        if [ "x${SSH_TTY}" != "x" ]; then
-                # prompt coloré
-                rouge=$(tput setaf 1)
-                vert=$(tput setaf 2)
-                jaune=$(tput setaf 3)
-                bleu=$(tput setaf 4)
-                gras=$(tput bold)
-                reset=$(tput sgr0)
+    # eviter le prompt si ce n'est pas une connexion ssh en tty
+    if [ "x${SSH_TTY}" != "x" ]; then
+            # prompt coloré
+            rouge=$(tput setaf 1)
+            vert=$(tput setaf 2)
+            jaune=$(tput setaf 3)
+            bleu=$(tput setaf 4)
+            gras=$(tput bold)
+            reset=$(tput sgr0)
 
-                export PS1='[\[$jaune\]\u\[$reset\]@\[$vert\]\h\[$reset\] \[$rouge\]${TWO_TASK:-$ORACLE_SID}\[$reset\] \W]\$ '
-                export NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS'
-        fi
+            export PS1='[\[$jaune\]\u\[$reset\]@\[$vert\]\h\[$reset\] \[$rouge\]${TWO_TASK:-$ORACLE_SID}\[$reset\] \W]\$ '
+            export NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS'
     fi
 fi
 
