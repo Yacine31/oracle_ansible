6b2cd60e81502d8ec0d2d6c5e484836a3d7fe888
Yacine31 <yacine.oumghar@gmail.com>
2022-03-16 03:18:44 +0100

    Update z_oracle.sh

--- roles/configure_os/files/z_oracle.sh
+++ roles/configure_os/files/z_oracle.sh
@@ -24,7 +24,7 @@ if [ $USER = "grid" ] || [ $USER = "oracle" ] || [ $USER = "root" ] ; then
 
         # export ORACLE_HOME et ORACLE_SID
         ORACLE_SID=$(ps -ef | grep pmon | egrep -v 'grep|ASM|APX' | cut -d_ -f3 | head -1)
-        if [ -z "$ORACLE_SID" ]; then export ORACLE_SID; fi
+        if [ -n "$ORACLE_SID" ]; then export ORACLE_SID; fi
         if [ -e "/etc/oraInst.loc" ]; then
             INV_LOC=$(cat /etc/oraInst.loc | grep inventory_loc | cut -d= -f2)
             if [ -e "${INV_LOC}/ContentsXML/inventory.xml" ]; then
