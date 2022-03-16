2840a009a218536de0fd8ac750b53c513398c051
Yacine31 <yacine.oumghar@gmail.com>
2022-03-16 02:31:47 +0100

    Update z_oracle.sh

--- roles/configure_os/files/z_oracle.sh
+++ roles/configure_os/files/z_oracle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 ## On entre seulement pour certains utilisateurs.
 if [ $USER = "grid" ] || [ $USER = "oracle" ] || [ $USER = "root" ] ; then
     
@@ -21,6 +22,13 @@ if [ $USER = "grid" ] || [ $USER = "oracle" ] || [ $USER = "root" ] ; then
         # Masque de création des fichiers    
         umask 022
 
+        # export ORACLE_HOME et ORACLe_SID
+        export ORACLE_SID=$(ps -ef | grep pmon | egrep -v 'grep|ASM|APX' | cut -d_ -f3)
+        INV_LOC=$(cat /etc/oraInst.loc | grep inventory_loc | cut -d= -f2)
+        export ORACLE_HOME=$(cat ${INV_LOC}/ContentsXML/inventory.xml | grep "<HOME_LIST" -A1 | tail -1 | sed 's/.*LOC="//g' | cut -d'"' -f1)
+        export PATH=$ORACLE_HOME/bin:$PATH
+
+
         # Alias RL Wrap si disponible
         if [ `type rlwrapp 2>/dev/null | wc -l` -eq 1 ] ; then
             alias sqlplus="rlwrap sqlplus"
@@ -31,9 +39,12 @@ if [ $USER = "grid" ] || [ $USER = "oracle" ] || [ $USER = "root" ] ; then
         fi
     fi
         
-    # eviter le prompt si ce n'est pas une connexion ssh en tty
-    if [ -t 0 ]; then
-        # prompt coloré
+    # Mise en place d'un prompt coloré
+    fd=0
+    # Shell interactif ou non ?
+    # Aucun intérêt hors d'un terminal interactif
+    if [[ $- = *i* ]]
+    then
         rouge=$(tput setaf 1)
         vert=$(tput setaf 2)
         jaune=$(tput setaf 3)
